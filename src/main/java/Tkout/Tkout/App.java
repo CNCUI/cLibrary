ec159dfd1375b20686d3d703ad8c7f92b210de28
ccui <847294832@qq.com>
2018-01-19 12:10:14 +0800

    1

---
+++ src/main/java/Tkout/Tkout/App.java
@@ -0,0 +1,13 @@
+package Tkout.Tkout;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
